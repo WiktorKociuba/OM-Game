cbcd8d014930a438b2357b22ddba302b7f20c41c
Wiktor Kociuba <77389227+WiktorKociuba@users.noreply.github.com>
2025-09-27 21:46:36 +0200

    Organized FileSystem + added readme

---
+++ readme.md
@@ -0,0 +1,8 @@
+# OM Game
+A 2D game made for [**Daydream Game Jam**](https://daydream.hackclub.com/) where you have to sacrifice your FOV in order to complete the game. Playable via web on [**itch.io**](https://mejpek.itch.io/om-game), no need to download anything! 
+## How to play
+Player can move using WASD or →←↓↑ and attack the enemies with left mouse button. Goal of the game is to defeat the final boss, that loses HP based on the players level. Have fun!
+### Contributors
+[VanClomp](https://github.com/VanClomp)
+[Wiktor573](https://github.com/Wiktor573)
+[WiktorKociuba](https://github.com/WiktorKociuba)
--- script/player.gd
+++ script/player.gd
@@ -2,32 +2,53 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
+@export var health = 5
+@export var hearthSprite: Texture2D
 @export var enemyScene: PackedScene
-@export var spawnRadius: float = 200.0
-var bossFight = false
+@export var spawnRadius: float = 500.0
+@onready var cam = $Camera2D
+var base_zoom = Vector2(1,1)
 
-func _input(event):
-	if event.is_action_pressed("attack"):
-		$AnimatedSprite2D.play("attack")
-		
+var maxHealth = 5
+var healthRestore = 0
+var enemyspawn = 0
 
 func spawnEnemy():
-	if bossFight:
-		pass
 	var rng = RandomNumberGenerator.new()
 	var angle = rng.randf_range(0,TAU)
-	var distance = rng.randf_range(0,spawnRadius)
+	var distance = rng.randf_range(200,spawnRadius)
 	var offset = Vector2(cos(angle), sin(angle)) * distance
 	var spawnPos = global_position + offset
 	var enemy = enemyScene.instantiate()
 	enemy.global_position = spawnPos
 	get_tree().current_scene.add_child(enemy)
+
+func _input(event):
+	if event.is_action_pressed("attack"):
+		$AnimatedSprite2D.play("attack")
+
+func _process(delta: float) -> void:
+	if health < 5:
+		if Time.get_ticks_msec() - healthRestore >= 30000:
+			healthRestore = Time.get_ticks_msec()
+			health += 1
+	if health <= 0:
+		get_tree().change_scene_to_file("res://scenes/main.tscn")
+	for i in range(maxHealth):
+		var heartNode = $Camera2D/HealthBar.get_child(i)
+		if i < health:
+			heartNode.visible = true
+		else:
+			heartNode.visible = false
+	if Time.get_ticks_msec() - enemyspawn >= 10000:
+		enemyspawn = Time.get_ticks_msec()
+		spawnEnemy()
+	var percent = clamp(100-(GameState.level*5), 30, 100)
+	var factor = 100.0/percent
+	cam.zoom = base_zoom * factor
 	
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction_x := Input.get_axis("left", "right")
@@ -50,3 +71,5 @@ func _physics_process(delta: float) -> void:
 		$AnimatedSprite2D.play("walk")
 
 	move_and_slide()
+
+		
