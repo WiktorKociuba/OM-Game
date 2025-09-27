e0a95ae267da8d606bc64a9975cc54cc2c60d261
Wiktor Kociuba <77389227+WiktorKociuba@users.noreply.github.com>
2025-09-27 17:52:29 +0200

    add boss health

---
+++ script/enemy_2.gd
@@ -0,0 +1,34 @@
+extends RigidBody2D
+
+const SPEED = 75.0
+@export var hit = false
+var hit_count = 0
+var maxHit = 250
+
+func _ready() -> void:
+	lock_rotation = true
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	maxHit -= GameState.level
+	var player = get_node("/root/Node2D/Player")
+	var playerPos = player.position
+	if hit_count >= maxHit:
+		GameState.add_points(200)
+		queue_free()
+	if player and not hit:
+		var direction = (playerPos-position).normalized()
+		
+		linear_velocity = direction * SPEED
+	elif hit:
+		var direction = (position-playerPos).normalized()
+		linear_velocity = direction * SPEED 
+		await get_tree().create_timer(1.0).timeout
+		hit = false
+		
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		print("Decrease health")
+		body.health -= 1
+		await get_tree().create_timer(3.0).timeout
